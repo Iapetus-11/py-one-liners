547249ed719571bb74876b8d328417c67a61d7b1
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2020-08-28 14:54:48 -0400

    Add prompt for length of sequence to calculate

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,1 +1,1 @@
-print([(lambda a:lambda b:a(a, b))(lambda r, n: n-1 if n < 3 else (r(r, n-1)+r(r, n-2)))(i) for i in range(1, 10)])
+print([(lambda a:lambda b:a(a, b))(lambda r, n: n-1 if n < 3 else (r(r, n-1)+r(r, n-2)))(i) for i in range(1, int(input('pick the number of digits to calculate: ')))])
