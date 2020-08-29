17ae7b30e78c2e066887b88a36a0fb8f5f8992f0
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2020-08-28 20:25:30 -0400

    Update fibonacci_sequence_better.py

--- fibonacci_sequence_better.py
+++ fibonacci_sequence_better.py
@@ -1,1 +1,1 @@
-f = (lambda r, n: n-1 if n<3 else r(r, n-1) + r(r, n-2)); print([f(f, i) for i in range(int(input('Pick the amount of numbers in the Fibonacci sequence to calculate: ')))])
+f = (lambda r, n: n-1 if n<3 else r(r, n-1) + r(r, n-2)); print([f(f, i) for i in range(int(input('Enter the number of numbers in the Fibonacii sequence to calculate: ')))])
