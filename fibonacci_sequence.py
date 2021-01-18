fcc2a0a079e9c87d9e14b0226a5349e0941f3da3
Milo Weinberg <iapetus011@gmail.com>
2021-01-17 19:39:15 -0500

    remove some stuff

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,1 +1,1 @@
-print([(lambda a:lambda b:a(a, b))(lambda r, n: n-1 if n < 3 else (r(r, n-1)+r(r, n-2)))(i) for i in range(1, int(input('pick the number of digits to calculate: ')))])
+f = (lambda r, n: n-1 if n<3 else r(r, n-1) + r(r, n-2)); print([f(f, i) for i in range(int(input('Enter the number of numbers in the Fibonacii sequence to calculate: ')))])
--- fibonacci_sequence_better.py
+++
@@ -1,1 +0,0 @@
-f = (lambda r, n: n-1 if n<3 else r(r, n-1) + r(r, n-2)); print([f(f, i) for i in range(int(input('Enter the number of numbers in the Fibonacii sequence to calculate: ')))])
